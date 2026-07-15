6bbaeb21caa302eaa46d35921150c68a94361b1b
James Liu <68935116+jameshualiu@users.noreply.github.com>
2026-07-15 16:19:11 -0700

    Add: baseball-game - submission-7

---
+++ Data Structures & Algorithms/baseball-game/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> scores;
+        for(int i = 0; i < operations.size(); ++i) {
+            string s = operations[i];
+            if(s == "D") {
+                scores.push_back(scores[scores.size() - 1] * 2);
+            } else if(s == "C") {
+                scores.pop_back();
+            } else if(s == "+") {
+                scores.push_back(scores[scores.size() - 1] + scores[scores.size() - 2]);
+            } else {
+                scores.push_back(stoi(operations[i]));
+            }
+        }
+        int ans = 0;
+        for(int i : scores)
+            ans += i;
+        return ans;
+    }
+};
